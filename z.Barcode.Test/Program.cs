325801187d52ceaf663a5fba1b4a9f5d45f2b653
Lyndon Jose Villareal <lyndonjose.villareal@infor.com>
2022-08-30 10:31:51 +0800

    Implement QR with custom middle image

--- z.Barcode.Test/Program.cs
+++ z.Barcode.Test/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using z.Barcode.QrCode.Internal;
-using z.Barcode.Rendering;
 
 namespace z.Barcode.Test
 {
@@ -30,35 +28,15 @@ namespace z.Barcode.Test
 
         static void GenerateZXingQRCode()
         {
-            var bw = new BarcodeWriter();
-
-            var encOptions = new Common.EncodingOptions
-            {
-                Width = 500,
-                Height = 500,
-                Margin = 3,
-                PureBarcode = false
-            };
-
-            encOptions.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-
-            bw.Renderer = new BitmapRenderer();
-            bw.Options = encOptions;
-            bw.Format = BarcodeFormat.QR_CODE;
-            Bitmap bm = bw.Write("Hello fuckers");
-
+           
             var mpc = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             var img = Path.Combine(mpc, "Resources", "Picture1.png");
 
             Bitmap overlay = new Bitmap(img);
-
-            int deltaHeigth = bm.Height - overlay.Height;
-            int deltaWidth = bm.Width - overlay.Width;
-
-            Graphics g = Graphics.FromImage(bm);
-            g.DrawImage(overlay, new Point(deltaWidth / 2, deltaHeigth / 2));
-
-            bm.Save($@"c:\temp\QR-{Guid.NewGuid().ToString("N")}.jpg");
+ 
+            var qrCode = new QRWriter();
+            var bmp = qrCode.Create("Hello Fuckers").GetGraphic(overlay, iconBorderWidth: 5);
+            bmp.Save($@"c:\temp\QR-{Guid.NewGuid().ToString("N")}.jpg");
         }
     }
 }
---
+++ z.Barcode/QRWriter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using z.Barcode.QrCode.Internal;
+using z.Barcode.Rendering;
+
+namespace z.Barcode
+{
+    public class QRWriter
+    {
+        private Bitmap QRBitmap;
+
+        public QRWriter Create(string data, int width = 500, int height = 500, int margin = 3)
+        {
+            var bw = new BarcodeWriter();
+
+            var encOptions = new Common.EncodingOptions
+            {
+                Width = width,
+                Height = height,
+                Margin = margin,
+                PureBarcode = false
+            };
+
+            encOptions.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+            bw.Renderer = new BitmapRenderer();
+            bw.Options = encOptions;
+            bw.Format = BarcodeFormat.QR_CODE;
+
+            QRBitmap = bw.Write(data);
+
+            return this;
+        }
+
+        public Bitmap GetGraphic() => QRBitmap;
+
+        public Bitmap GetGraphic(Bitmap icon = null, int iconSizePercent = 15, int iconBorderWidth = 0, Color? iconBackgroundColor = null)
+        { 
+            var drawIconFlag = icon != null && iconSizePercent > 0 && iconSizePercent <= 100;
+
+            if (!drawIconFlag)
+                return GetGraphic();
+
+            using (var gfx = Graphics.FromImage(QRBitmap))
+            { 
+                var iconDestWidth = iconSizePercent * QRBitmap.Width / 100f;
+                var iconDestHeight = drawIconFlag ? iconDestWidth * icon.Height / icon.Width : 0;
+                var iconX = (QRBitmap.Width - iconDestWidth) / 2;
+                var iconY = (QRBitmap.Height - iconDestHeight) / 2;
+                var centerDest = new RectangleF(iconX - iconBorderWidth, iconY - iconBorderWidth, iconDestWidth +iconBorderWidth * 2, iconDestWidth + iconBorderWidth * 2);
+                var iconDestRect = new RectangleF(iconX, iconY, iconDestWidth, iconDestHeight);
+                var iconBgBrush = iconBackgroundColor != null ? new SolidBrush((Color)iconBackgroundColor): new SolidBrush(Color.White);
+
+                //only render icon/logo background, if iconborderWitdh is set > 0
+                if(iconBorderWidth > 0)
+                {
+                    using(var iconPath = CreateRoundedRectanglePath(centerDest, iconBorderWidth * 2) )
+                    {
+                        gfx.FillPath(iconBgBrush, iconPath);
+                    }
+                }
+                gfx.DrawImage(icon, iconDestRect, new RectangleF(0, 0, icon.Width, icon.Height), GraphicsUnit.Pixel);
+                gfx.Save();
+            }
+            return QRBitmap;
+        }
+
+        internal GraphicsPath CreateRoundedRectanglePath(RectangleF rect, int cornerRadius)
+        {
+            var roundedRect = new GraphicsPath();
+            roundedRect.AddArc(rect.X, rect.Y, cornerRadius * 2, cornerRadius * 2, 180, 90);
+            roundedRect.AddLine(rect.X + cornerRadius, rect.Y, rect.Right - cornerRadius * 2, rect.Y);
+            roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y, cornerRadius * 2, cornerRadius * 2, 270, 90);
+            roundedRect.AddLine(rect.Right, rect.Y + cornerRadius * 2, rect.Right, rect.Y + rect.Height - cornerRadius * 2);
+            roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y + rect.Height - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 0, 90);
+            roundedRect.AddLine(rect.Right - cornerRadius * 2, rect.Bottom, rect.X + cornerRadius * 2, rect.Bottom);
+            roundedRect.AddArc(rect.X, rect.Bottom - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 90, 90);
+            roundedRect.AddLine(rect.X, rect.Bottom - cornerRadius * 2, rect.X, rect.Y + cornerRadius * 2);
+            roundedRect.CloseFigure();
+            return roundedRect;
+        }
+    }
+}
